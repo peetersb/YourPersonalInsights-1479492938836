765dd96513e0363917fe61c05d96e592c11f0836
Brendt Peeters <peetersb@schneider.com>
2016-11-18 12:34:52 -0600

    Setting up the framework.

--- index.php
+++ index.php
@@ -1,21 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PHP Starter Application</title>
+	<title>SCHNEIDER - Personal Finance Insights</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+	<link ref="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="style.css" />
+	<script>
+	$("#start").click(function(){
+		$("#phase1").slideUp("fast");
+		$("#phase2").slideDown("fast");
+	});
+	</script>
 </head>
 <body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
+	<div id="phase1">
+		<table>
+			<tr>
+				<td style='width: 30%;'>
+					<i class="fa fa-line-chart fa-5x" aria-hidden="true"></i>
+				</td>
+				<td>
+					<h1 id = "message">Get Started On Your Future Today</h1>
+					<p class='description'></p>When you are ready <span id="start" class="blue">you should click here</span>.
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div id="phase2">
+		<h1>Enter your details!</h1>
+		<table>
+			<tr>
+				<td>Name:</td>
+				<td><input type="text" name="name" id="name"></td>
+			</tr>
+		</table>
+		<em>No insights provided by this tool are binding</em>
+	</div>
 </body>
 </html>
